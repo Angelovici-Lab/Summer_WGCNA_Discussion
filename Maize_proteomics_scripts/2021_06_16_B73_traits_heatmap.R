49c92e14e054d466af5385a86355a199c49bc550
Yen On Chan <yenon118@gmail.com>
2021-06-16 23:59:40 -0500

    Added heatmap, kME, hub genes, and module ovelapping code

---
+++ Maize_proteomics_scripts/2021_06_16_B73_traits_heatmap.R
@@ -0,0 +1,220 @@
+rm(list=ls())
+
+library(jpeg)
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(stringr)
+library(ggplot2)
+
+library(foreach)
+library(iterators)
+library(parallel)
+library(doParallel)
+
+library(WGCNA)
+# library(KEGGREST)
+# library(biomaRt)
+
+set.seed(1)
+
+# Enable WGCNA threads to speed up calculations
+enableWGCNAThreads()
+
+
+#######################################################################
+# Constants/Variables
+#######################################################################
+selected_genotype <- "B73"
+softPower <- 14
+
+# Number to top hub genes
+nTop = 30
+
+
+##################################################
+# Output folder
+##################################################
+output_path <- file.path(
+  paste0(
+    "/home/ycth8/data/projects/2021_05_30_summer_WGCNA/Maize_proteomics_output/",
+    paste0("2021_06_16_", selected_genotype, "_traits_heatmap")
+  )
+)
+
+if(!dir.exists(output_path)){
+  dir.create(output_path, showWarnings=FALSE, recursive=TRUE)
+  if(!dir.exists(output_path)){
+    quit(status=1)
+  }
+}
+
+
+##################################################
+# Read in input file
+##################################################
+
+folder_path = file.path("/home/ycth8/data/projects/2021_05_30_summer_WGCNA/Maize_proteomics_output")
+
+datTraits = read.csv(
+  file = file.path(folder_path, "datTraits.csv"),
+  header = TRUE,
+  row.names = 1,
+  check.names = FALSE,
+  stringsAsFactors = FALSE
+)
+
+datTraits = datTraits[startsWith(rownames(datTraits), selected_genotype),]
+
+
+lnames = load(
+  file = file.path(
+    folder_path,
+    paste0("2021_06_10_", selected_genotype, "_step_by_step_network_construction"),
+    paste0(selected_genotype, "-networkConstruction-stepByStep.RData")
+  )
+)
+print(lnames)
+
+
+# Define numbers of genes and samples
+nGenes = ncol(datExpr)
+nSamples = nrow(datExpr)
+
+# Recalculate MEs with color labels
+MEs0 = moduleEigengenes(datExpr, moduleColors)$eigengenes
+MEs = orderMEs(MEs0)
+
+moduleTraitCor = cor(MEs, datTraits, use = "p")
+moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples)
+
+
+textMatrix = paste0(signif(moduleTraitCor, 2), "\n(", signif(moduleTraitPvalue, 1), ")");
+dim(textMatrix) = dim(moduleTraitCor)
+
+modTotals = c()
+for(i in 1:length(names(MEs))){
+  modTotals = c(modTotals, length(moduleColors[moduleColors == substring(names(MEs)[i], 3)]))
+}
+
+cat(rep("\n", 2))
+jpeg(filename = file.path(output_path, paste0(selected_genotype, "_PBAA_modules_absolute_traits_correlation.jpeg")),
+     pointsize = 15, quality =95, height = 480*1.5, width = 480*1.7)
+par(mar = c(5, 10, 3, 3))
+labeledHeatmap(
+  Matrix = moduleTraitCor,
+  xLabels = names(datTraits),
+  yLabels = names(MEs),
+  ySymbols = paste0(names(MEs), ": ", modTotals),
+  colorLabels = FALSE,
+  colors = greenWhiteRed(50),
+  textMatrix = textMatrix,
+  setStdMargins = FALSE,
+  cex.text = 0.5,
+  zlim = c(-1,1),
+  main = paste(selected_genotype, " module-absolute_trait relationships")
+)
+dev.off()
+
+
+##################################################
+# Get top hub gene using WGCNA chooseTopHubInEachModule
+##################################################
+top_hub <- chooseTopHubInEachModule(datExpr, moduleColors, omitColors = "")
+
+top_hub_df <- data.frame(
+  "Module" = names(top_hub),
+  "Hub_Gene" = as.vector(top_hub),
+  stringsAsFactors = FALSE
+)
+
+write.csv(
+  x = top_hub_df,
+  file = file.path(output_path, paste0(selected_genotype, "_top_hub_df.csv")),
+  na = "",
+  quote = FALSE,
+  row.names = FALSE
+)
+
+
+##################################################
+# Calculate kME
+##################################################
+signed_KME_df <- as.data.frame(signedKME(datExpr, MEs), stringsAsFactors = FALSE) %>%
+  rownames_to_column(var = "Gene") %>%
+  as.data.frame(stringsAsFactors = FALSE) %>%
+  pivot_longer(!Gene, names_to = "Color", values_to = "kME") %>%
+  mutate(Color = gsub("^kME", "", Color)) %>%
+  arrange(desc(kME)) %>%
+  arrange(desc(abs(kME))) %>%
+  as.data.frame(stringsAsFactors = FALSE)
+
+write.csv(
+  x = signed_KME_df,
+  file = file.path(output_path, paste0(selected_genotype, "_kME_df.csv")),
+  na = "",
+  quote = FALSE,
+  row.names = FALSE
+)
+
+
+##################################################
+# Calculate soft connectivity
+##################################################
+IMConn = softConnectivity(datExpr)
+
+IMConn_df <- data.frame(
+  "Gene" = names(datExpr),
+  "SoftConnectivity" = IMConn,
+  stringsAsFactors = FALSE
+)
+
+IMConn_df = IMConn_df %>%
+  arrange(desc(SoftConnectivity)) %>%
+  as.data.frame(stringsAsFactors = FALSE)
+
+write.csv(
+  x = IMConn_df,
+  file = file.path(output_path, paste0(selected_genotype, "_softConnectivity.csv")),
+  na = "",
+  quote = FALSE,
+  row.names = FALSE
+)
+
+top = (rank(-IMConn) <= nTop)
+
+
+##################################################
+# Export nodes and edges
+##################################################
+# Select modules
+# modules = c("brown", "red")
+modules = unique(moduleColors)
+
+# Recalculate topological overlap if needed
+TOM = TOMsimilarityFromExpr(datExpr, power = softPower)
+
+# Select module probes
+probes = names(datExpr)
+inModule = is.finite(match(moduleColors, modules))
+modProbes = probes[inModule]
+modGenes = modProbes
+
+# Select the corresponding Topological Overlap
+modTOM = TOM[inModule, inModule]
+
+dimnames(modTOM) = list(modProbes, modProbes)
+
+# print(head(modTOM))
+
+# Export the network into edge and node list files Cytoscape can read
+cyt = exportNetworkToCytoscape(
+  modTOM,
+  edgeFile = file.path(output_path, paste0(selected_genotype, "_CytoscapeInput-edges-", paste(modules, collapse = "-"), ".txt")),
+  nodeFile = file.path(output_path, paste0(selected_genotype, "_CytoscapeInput-nodes-", paste(modules, collapse = "-"), ".txt")),
+  weighted = TRUE,
+  threshold = 0.02,
+  nodeNames = modProbes,
+  altNodeNames = modGenes,
+  nodeAttr = moduleColors[inModule]
+)
---
+++ Maize_proteomics_scripts/2021_06_16_overlap_B73_O2_modules.R
@@ -0,0 +1,171 @@
+rm(list=ls())
+
+library(jpeg)
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(stringr)
+library(ggplot2)
+
+library(foreach)
+library(iterators)
+library(parallel)
+library(doParallel)
+
+library(WGCNA)
+# library(KEGGREST)
+# library(biomaRt)
+
+set.seed(1)
+
+# Enable WGCNA threads to speed up calculations
+enableWGCNAThreads()
+
+
+#######################################################################
+# Constants/Variables
+#######################################################################
+
+
+##################################################
+# Output folder
+##################################################
+output_path <- file.path(
+  "/home/ycth8/data/projects/2021_05_30_summer_WGCNA/Maize_proteomics_output/2021_06_16_overlap_B73_O2_modules"
+)
+
+if(!dir.exists(output_path)){
+  dir.create(output_path, showWarnings=FALSE, recursive=TRUE)
+  if(!dir.exists(output_path)){
+    quit(status=1)
+  }
+}
+
+
+##################################################
+# Read in input file
+##################################################
+
+folder_path = file.path("/home/ycth8/data/projects/2021_05_30_summer_WGCNA/Maize_proteomics_output")
+
+# datTraits = read.csv(
+#   file = file.path(folder_path, "datTraits.csv"),
+#   header = TRUE,
+#   row.names = 1,
+#   check.names = FALSE,
+#   stringsAsFactors = FALSE
+# )
+#
+# datTraits = datTraits[startsWith(rownames(datTraits), selected_genotype),]
+
+selected_genotype <- "B73"
+lnames = load(
+  file = file.path(
+    folder_path,
+    paste0("2021_06_10_", selected_genotype, "_step_by_step_network_construction"),
+    paste0(selected_genotype, "-networkConstruction-stepByStep.RData")
+  )
+)
+print(lnames)
+
+# Replace B73 realted variable names
+b73Expr = datExpr
+
+index <- match(sub("^ME", "", colnames(MEs)), moduleColors)
+
+colnames(MEs) <- paste0("ME", moduleLabels[index])
+
+b73MEs = orderMEs(MEs, greyName = "ME0")
+b73Labels = moduleLabels
+b73Colors = moduleColors
+b73Tree = geneTree
+
+cat(rep("\n", 2))
+print(head(b73Labels))
+print(head(b73Colors))
+print(head(b73MEs))
+
+
+selected_genotype <- "O2"
+lnames = load(
+  file = file.path(
+    folder_path,
+    paste0("2021_06_10_", selected_genotype, "_step_by_step_network_construction"),
+    paste0(selected_genotype, "-networkConstruction-stepByStep.RData")
+  )
+)
+print(lnames)
+
+
+# Replace O2 realted variable names
+o2Expr = datExpr
+
+index <- match(sub("^ME", "", colnames(MEs)), moduleColors)
+
+colnames(MEs) <- paste0("ME", moduleLabels[index])
+
+o2MEs = orderMEs(MEs, greyName = "ME0")
+o2Labels = moduleLabels
+o2Colors = moduleColors
+o2Tree = geneTree
+
+cat(rep("\n", 2))
+print(head(o2Labels))
+print(head(o2Colors))
+print(head(o2MEs))
+
+
+# Isolate the module labels in the order they appear in ordered module eigengenes
+b73ModuleLabels = substring(names(b73MEs), 3)
+o2ModuleLabels = substring(names(o2MEs), 3)
+
+
+# Convert the numeric module labels to color labels
+b73Modules = labels2colors(as.numeric(b73ModuleLabels))
+o2Modules = labels2colors(as.numeric(o2ModuleLabels))
+
+# Numbers of B73 and O2 modules
+nB73Mods = length(b73Modules)
+nO2Mods = length(o2Modules)
+
+# Initialize tables of p-values and of the corresponding counts
+# pTable = matrix(0, nrow = nB73Mods, ncol = nO2Mods)
+CountTbl = matrix(0, nrow = nB73Mods, ncol = nO2Mods)
+
+
+for (i in 1:nB73Mods) {
+  for (cmod in 1:nO2Mods) {
+    b73Members = (b73Colors == b73Modules[i])
+    o2Members = (o2Colors == o2Modules[cmod])
+    # pTable[i, cmod] = -log10(fisher.test(b73Members, o2Members, alternative = "greater")$p.value)
+    CountTbl[i, cmod] = sum(b73Colors == b73Modules[i] & o2Colors == o2Modules[cmod])
+  }
+}
+
+
+# Truncate p values smaller than 10^{-50} to 10^{-50}
+# pTable[is.infinite(pTable)] = 1.3*max(pTable[is.finite(pTable)])
+# pTable[pTable>50 ] = 50
+
+# Marginal counts (really module sizes)
+b73ModTotals = apply(CountTbl, 1, sum)
+o2ModTotals = apply(CountTbl, 2, sum)
+
+
+cat(rep("\n", 2))
+jpeg(file = file.path(output_path, "B73_vs_O2.jpg"), height = 480*2, width = 480*2)
+par(mfrow=c(1,1))
+par(cex = 1.0)
+par(mar=c(10, 12, 3, 1))
+labeledHeatmap(Matrix = CountTbl,
+               xLabels = paste(" ", o2Modules),
+               yLabels = paste(" ", b73Modules),
+               colorLabels = TRUE,
+               xSymbols = paste0("O2 ", o2Modules, ": ", o2ModTotals),
+               ySymbols = paste0("B73 ", b73Modules, ": ", b73ModTotals),
+               textMatrix = CountTbl,
+               colors = greenWhiteRed(100)[50:100],
+               main = "Correspondence of B73 set-specific and O2 set-specific modules",
+               cex.text = 1.0, cex.lab = 1.0, setStdMargins = FALSE)
+
+dev.off()
